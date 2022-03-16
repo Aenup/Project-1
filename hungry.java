52e5c2e4ac083fd7699de51cc443554e030e6e45
abc <you@gmail.com>
2022-03-16 17:29:38 +0530

    second commit

--- hungry.java
+++ hungry.java
@@ -2,6 +2,9 @@ import java.util.Scanner;
 
 class hungry{
     public static void main(String args[]){
+        
+
+        System.out.print("Do work or ");
         System.out.println("Hungry:-> Yes or No");
         
     }
